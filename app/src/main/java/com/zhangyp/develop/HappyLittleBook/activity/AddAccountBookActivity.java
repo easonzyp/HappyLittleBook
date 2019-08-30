eb488c3cf5bd4493b96713afabb5ca9bfb2443b4
easonzyp <735017618@qq.com>
2019-08-30 10:22:42 +0800

    重构添加收支信息功能

--- app/src/main/java/com/zhangyp/develop/HappyLittleBook/activity/AddAccountBookActivity.java
+++ app/src/main/java/com/zhangyp/develop/HappyLittleBook/activity/AddAccountBookActivity.java
@@ -5,7 +5,6 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.zhangyp.develop.HappyLittleBook.ExampleApplication;
@@ -14,6 +13,8 @@ import com.zhangyp.develop.HappyLittleBook.base.BaseActivity;
 import com.zhangyp.develop.HappyLittleBook.bean.AccountBookInfo;
 import com.zhangyp.develop.HappyLittleBook.bean.ExpendLevelOneCate;
 import com.zhangyp.develop.HappyLittleBook.bean.ExpendLevelTwoCate;
+import com.zhangyp.develop.HappyLittleBook.bean.IncomeLevelOneCate;
+import com.zhangyp.develop.HappyLittleBook.bean.IncomeLevelTwoCate;
 import com.zhangyp.develop.HappyLittleBook.db.DaoSession;
 import com.zhangyp.develop.HappyLittleBook.util.BasisTimesUtils;
 import com.zhangyp.develop.HappyLittleBook.util.ToastUtil;
@@ -27,7 +28,6 @@ public class AddAccountBookActivity extends BaseActivity {
     private DaoSession daoSession;
 
     private TextView tv_back;
-    private RelativeLayout rl_title;
     private EditText et_money;
     private TextView tv_time;
     private EditText et_note;
@@ -38,13 +38,17 @@ public class AddAccountBookActivity extends BaseActivity {
 
     private String timeStr;
 
-    private List<ExpendLevelOneCate> oneCateList;
+    private List<ExpendLevelOneCate> expendOneCateList;
+    private List<IncomeLevelOneCate> incomeOneCateList;
+    private ExpendLevelOneCate expendOneCate;
+    private IncomeLevelOneCate incomeOneCate;
     private List<String> oneCateNameList;
-    private ExpendLevelOneCate oneCate;
 
-    private List<ExpendLevelTwoCate> twoCateList;
+    private List<ExpendLevelTwoCate> expendTwoCateList;
+    private List<IncomeLevelTwoCate> incomeTwoCateList;
+    private ExpendLevelTwoCate expendTwoCate;
+    private IncomeLevelTwoCate incomeTwoCate;
     private List<String> twoCateNameList;
-    private ExpendLevelTwoCate twoCate;
 
     private int bookType;   //0:支出  1:收入
 
@@ -63,12 +67,10 @@ public class AddAccountBookActivity extends BaseActivity {
 
     private void initView() {
         daoSession = ((ExampleApplication) getApplication()).getDaoSession();
-        oneCateList = new ArrayList<>();
-        twoCateList = new ArrayList<>();
+
         oneCateNameList = new ArrayList<>();
         twoCateNameList = new ArrayList<>();
         tv_back = findViewById(R.id.tv_back);
-        rl_title = findViewById(R.id.rl_title);
         et_money = findViewById(R.id.et_money);
         tv_time = findViewById(R.id.tv_time);
         et_note = findViewById(R.id.et_note);
@@ -79,48 +81,47 @@ public class AddAccountBookActivity extends BaseActivity {
 
         if (bookType == 0) {
             tv_back.setText("添加支出");
+            expendOneCateList = new ArrayList<>();
+            expendTwoCateList = new ArrayList<>();
         } else {
             tv_back.setText("添加收入");
+            incomeOneCateList = new ArrayList<>();
+            incomeTwoCateList = new ArrayList<>();
         }
 
         tv_time.setText(BasisTimesUtils.getDeviceTime());
     }
 
     private void initData() {
-        getOneCateList();
+        if (bookType == 0) {
+            getExpendOneCateList();
+        } else {
+            getIncomeOneCateList();
+        }
     }
 
     private void initClick() {
+        tv_back.setOnClickListener(v -> finish());
+
         wll_one_cate.setMarkClickListener((text, position) -> {
-            oneCate = oneCateList.get(position);
-            getTwoCateList(oneCate.getId());
+            if (bookType == 0) {
+                expendOneCate = expendOneCateList.get(position);
+                getExpendTwoCateList(expendOneCate.getId());
+            } else {
+                incomeOneCate = incomeOneCateList.get(position);
+                getIncomeTwoCateList(incomeOneCate.getId());
+            }
         });
 
-        wll_two_cate.setMarkClickListener((text, position) -> twoCate = twoCateList.get(position));
-
-        tv_time.setOnClickListener(v -> BasisTimesUtils.showDatePickerDialog(context, "选择日期", new BasisTimesUtils.OnDatePickerListener() {
-            @Override
-            public void onConfirm(int year, int month, int dayOfMonth) {
-                timeStr = year + "年" + month + "月" + dayOfMonth + "日 ";
-                BasisTimesUtils.showTimerPickerDialog(context, "选择时间", true, new BasisTimesUtils.OnTimerPickerListener() {
-                    @Override
-                    public void onConfirm(int hourOfDay, int minute) {
-                        timeStr += hourOfDay + ":" + minute;
-                        tv_time.setText(timeStr);
-                    }
-
-                    @Override
-                    public void onCancel() {
-
-                    }
-                });
+        wll_two_cate.setMarkClickListener((text, position) -> {
+            if (bookType == 0) {
+                expendTwoCate = expendTwoCateList.get(position);
+            } else {
+                incomeTwoCate = incomeTwoCateList.get(position);
             }
+        });
 
-            @Override
-            public void onCancel() {
-
-            }
-        }));
+        tv_time.setOnClickListener(v -> showDateView());
 
         tv_save.setOnClickListener(v -> {
             String money = et_money.getText().toString().trim();
@@ -128,16 +129,31 @@ public class AddAccountBookActivity extends BaseActivity {
                 ToastUtil.showShortToast(context, "请填写支出金额");
                 return;
             }
-
-            if (twoCate == null && oneCate == null) {
-                ToastUtil.showShortToast(context, "请选择支出分类");
-                return;
-            }
-            String cateName;
-            if (twoCate != null) {
-                cateName = twoCate.getCateName();
+            if (bookType == 0) {
+                if (expendTwoCate == null && expendOneCate == null) {
+                    ToastUtil.showShortToast(context, "请选择支出分类");
+                    return;
+                }
             } else {
-                cateName = oneCate.getCateName();
+                if (incomeTwoCate == null && incomeOneCate == null) {
+                    ToastUtil.showShortToast(context, "请选择支出分类");
+                    return;
+                }
+            }
+
+            String cateName;
+            if (bookType == 0) {
+                if (expendTwoCate != null) {
+                    cateName = expendTwoCate.getCateName();
+                } else {
+                    cateName = expendOneCate.getCateName();
+                }
+            } else {
+                if (incomeTwoCate != null) {
+                    cateName = incomeTwoCate.getCateName();
+                } else {
+                    cateName = incomeOneCate.getCateName();
+                }
             }
 
             AccountBookInfo bookInfo = new AccountBookInfo();
@@ -166,33 +182,92 @@ public class AddAccountBookActivity extends BaseActivity {
         });
     }
 
-    private void getOneCateList() {
-        oneCateList.clear();
+    private void getExpendOneCateList() {
+        expendOneCateList.clear();
         oneCateNameList.clear();
-        oneCateList.addAll(daoSession.loadAll(ExpendLevelOneCate.class));
-        if (oneCateList == null || oneCateList.size() == 0) {
+        expendOneCateList.addAll(daoSession.loadAll(ExpendLevelOneCate.class));
+        if (expendOneCateList == null || expendOneCateList.size() == 0) {
             return;
         }
-        for (int i = 0; i < oneCateList.size(); i++) {
-            oneCateNameList.add(oneCateList.get(i).getCateName());
+        for (int i = 0; i < expendOneCateList.size(); i++) {
+            oneCateNameList.add(expendOneCateList.get(i).getCateName());
         }
         wll_one_cate.setData(oneCateNameList, context, 13, 10, 5, 10, 5, 0, 5, 10, 5);
     }
 
-    private void getTwoCateList(long oneCateId) {
-        twoCate = null;
-        twoCateList.clear();
+    private void getIncomeOneCateList() {
+        incomeOneCateList.clear();
+        oneCateNameList.clear();
+        incomeOneCateList.addAll(daoSession.loadAll(IncomeLevelOneCate.class));
+        if (incomeOneCateList == null || incomeOneCateList.size() == 0) {
+            return;
+        }
+        for (int i = 0; i < incomeOneCateList.size(); i++) {
+            oneCateNameList.add(incomeOneCateList.get(i).getCateName());
+        }
+        wll_one_cate.setData(oneCateNameList, context, 13, 10, 5, 10, 5, 0, 5, 10, 5);
+    }
+
+    private void getExpendTwoCateList(long oneCateId) {
+        expendTwoCate = null;
+        expendTwoCateList.clear();
         twoCateNameList.clear();
-        twoCateList.addAll(daoSession.queryRaw(ExpendLevelTwoCate.class, "where ONE_CATE_ID = ?", String.valueOf(oneCateId)));
-        if (twoCateList == null || twoCateList.size() == 0) {
+        expendTwoCateList.addAll(daoSession.queryRaw(ExpendLevelTwoCate.class, "where ONE_CATE_ID = ?", String.valueOf(oneCateId)));
+        if (expendTwoCateList == null || expendTwoCateList.size() == 0) {
             ll_two_cate.setVisibility(View.GONE);
             return;
         }
         ll_two_cate.setVisibility(View.VISIBLE);
-        for (int i = 0; i < twoCateList.size(); i++) {
-            twoCateNameList.add(twoCateList.get(i).getCateName());
+        for (int i = 0; i < expendTwoCateList.size(); i++) {
+            twoCateNameList.add(expendTwoCateList.get(i).getCateName());
         }
         wll_two_cate.setData(twoCateNameList, context, 13, 10, 5, 10, 5, 0, 5, 10, 5);
+    }
+
+    private void getIncomeTwoCateList(long oneCateId) {
+        incomeTwoCate = null;
+        incomeTwoCateList.clear();
+        twoCateNameList.clear();
+        incomeTwoCateList.addAll(daoSession.queryRaw(IncomeLevelTwoCate.class, "where ONE_CATE_ID = ?", String.valueOf(oneCateId)));
+        if (incomeTwoCateList == null || incomeTwoCateList.size() == 0) {
+            ll_two_cate.setVisibility(View.GONE);
+            return;
+        }
+        ll_two_cate.setVisibility(View.VISIBLE);
+        for (int i = 0; i < incomeTwoCateList.size(); i++) {
+            twoCateNameList.add(incomeTwoCateList.get(i).getCateName());
+        }
+        wll_two_cate.setData(twoCateNameList, context, 13, 10, 5, 10, 5, 0, 5, 10, 5);
+    }
+
+    private void showDateView() {
+        BasisTimesUtils.showDatePickerDialog(context, "选择日期", new BasisTimesUtils.OnDatePickerListener() {
+            @Override
+            public void onConfirm(int year, int month, int dayOfMonth) {
+                timeStr = year + "年" + month + "月" + dayOfMonth + "日 ";
+                showTimeView();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+        });
+    }
+
+    private void showTimeView() {
+        BasisTimesUtils.showTimerPickerDialog(context, "选择时间", true, new BasisTimesUtils.OnTimerPickerListener() {
+            @Override
+            public void onConfirm(int hourOfDay, int minute) {
+                timeStr += hourOfDay + ":" + minute;
+                tv_time.setText(timeStr);
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+        });
     }
 
     @Override
