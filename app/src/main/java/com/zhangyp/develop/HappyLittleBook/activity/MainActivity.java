c735ca06b82327e309bfad122bf7e3ce6a4f7de9
easonzyp <735017618@qq.com>
2019-10-23 14:27:52 +0800

    格式化代码

--- app/src/main/java/com/zhangyp/develop/HappyLittleBook/activity/MainActivity.java
+++ app/src/main/java/com/zhangyp/develop/HappyLittleBook/activity/MainActivity.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.app.hubert.guide.NewbieGuide;
 import com.app.hubert.guide.model.GuidePage;
